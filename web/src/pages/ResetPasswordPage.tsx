a10c5b4374219534a43c6d401cd59b64c0d55b67
okanvatanci <okanvatanci@gmail.com>
2026-01-24 23:20:24 +0300

    fix: remove unused useEffect import from ResetPasswordPage

--- web/src/pages/ResetPasswordPage.tsx
+++ web/src/pages/ResetPasswordPage.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import { IoLockClosed, IoEye, IoEyeOff, IoCheckmarkCircle, IoMail } from 'react-icons/io5';
 
